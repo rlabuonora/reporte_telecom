f5576f21db20c8119b0dfc3fd4043e961526b8f2
Rafael La Buonora <info@rlabuonora.com>
2020-08-28 14:10:51 -0300

    Usa parametros al markdown

--- render_semestral.R
+++ render_semestral.R
@@ -1,1 +1,7 @@
-rmarkdown::render("semestral/index.Rmd", output_file = "Informe Semestral.docx")
+rmarkdown::render("semestral/index.Rmd", 
+                  params = list(
+                    FECHA_INFORME_SEMESTRAL = as.Date("2020-06-01"),
+                    FECHA_TITULO_STOCK = "Junio 2020",
+                    FECHA_TITULO_FLUJO = "Primer Semestre 2020",
+                  ), 
+                  output_file = "Informe Semestral.docx")
