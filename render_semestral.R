3e6e3656b3faf2c15e709f8d78edf508eb0ae8f3
Rafael La Buonora <info@rlabuonora.com>
2020-09-03 15:44:13 -0300

    Ajustes varios

--- render_semestral.R
+++ render_semestral.R
@@ -1,7 +1,2 @@
 rmarkdown::render("semestral/index.Rmd", 
-                  params = list(
-                    FECHA_INFORME_SEMESTRAL = as.Date("2020-06-01"),
-                    FECHA_TITULO_STOCK = "Junio 2020",
-                    FECHA_TITULO_FLUJO = "Primer Semestre 2020",
-                  ), 
                   output_file = "Informe Semestral.docx")
