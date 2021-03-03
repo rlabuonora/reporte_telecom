f48d822653f413d0890cbc5c5292e35906e8f4f9
Rafael La Buonora <info@rlabuonora.com>
2021-03-03 15:44:56 -0300

    Version 1

--- common.R
+++ common.R
@@ -17,10 +17,3 @@ options(scipen = 10000)
 
 pal <- brewer.pal(n = 8, name = "Blues")
 
-theme_set(theme_minimal() + 
-            theme(plot.background = element_rect(fill = "#EEEEEE",
-                                                 color = "white"),
-                  #axis.title = element_text(size = rel(1.5)),
-                  #axis.text = element_text(size = rel(1.5)),
-                  axis.line.x = element_blank(),
-                  axis.ticks = element_blank()))
